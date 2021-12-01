a7073182ee6c0fc1c86041a3d0fd5ed57e0416e9
RNagai <47627719+ml-electron-project@users.noreply.github.com>
2021-12-01 17:19:07 +0900

    Add files via upload

--- patch_implement_pcNN.py
+++ patch_implement_pcNN.py
@@ -1,21 +1,45 @@
 ############################
-# Patch to implement pcNN for VASP ver. 5.4.4. written by Ryo Nagai
-# email: r-nag@issp.u-tokyo.ac.jp
-#
+# pcNN_solid
+# VASP version of pcNN XC functional. This add-on is available only to owners of a valid VASP license.
 
-# This add-on is only available to owners of a valid VASP license.
+
+# ## requirements
+# VASP 5.4.4 with source code (It does not work with VASP 6.)
+# Python3
+# PyArmor
+
+
+# ## Installation
+# 1. Backup "vasp/src/metagga.F" to another directory.
+# 2. Run "patch_implement_pcNN.py". You will be asked the absolute path to metagga.F.
+# 3. "metagga_patched.F" will be generated. Rename it to "metagga.F" and replace "vasp/src/metagga.F" with it.
+# 4. Compile VASP.
+
+# ## Usage
+# 1. Place "nnparams" folder into the same directory as INCAR.
+
+# (example)
+# ```
+# (target folder) -- nnparams
+#                 -- INCAR
+#                 -- POSCAR
+#                 -- KPOINTS
+#                 -- POTCAR
+#                 ...
+# ```
+
+# 2. Add (or modify) the keyword "METAGGA = NNMGGA" to the INCAR.
+# 3. Run the calculation with the VASP program compiled in the Installation section.
+
+# ## Notification
 # This add-on is distributed under an agreement from VASP Software GmbH.
 
 # - This add-on comes without any waranty.
-# - Commercial use and patent use are prohibited.
-# - If the add-on is modified by inclusion of additional source code lines of the Add-on to the VASP software code,
-#   the developer must send the revised version of the add-on to VASP Software GmbH for further review.
+# - If the add-on is modified by inclusion of additional source code lines from the main VASP source code,
+# the developer must send the revised version of the add-on to VASP Software GmbH for further review.
 
-# ## Usage
-# 1. Backup vasp/src/metagga.F.
-# 2. Run this patch. You are asked the path to metagga.F.
-# 3. metagga_patched.F is generated. Replace metagga.F with it.
-# 4. Compile VASP.
+# Copyright 2021 Ryo Nagai
+
 
 # ## Citing this XC functinoal
 # @misc{nagai2021machinelearningbased,
@@ -26,6 +50,7 @@
 #       archivePrefix={arXiv},
 #       primaryClass={cond-mat.mtrl-sci}
 # }
+
 ############################
 
 
