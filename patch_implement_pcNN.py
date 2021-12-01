ee8b0815e82d36b4447a945c1d44818454bbe7bf
RNagai <47627719+ml-electron-project@users.noreply.github.com>
2021-12-01 17:30:47 +0900

    Add files via upload

--- patch_implement_pcNN.py
+++ patch_implement_pcNN.py
@@ -39,7 +39,17 @@
 # the developer must send the revised version of the add-on to VASP Software GmbH for further review.
 
 # Copyright 2021 Ryo Nagai
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
 
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
 
 # ## Citing this XC functinoal
 # @misc{nagai2021machinelearningbased,
